96103410e825e98c878ac29308aedb1bb57fdc58
citrus327 <citrus327@outlook.com>
2024-02-28 14:07:58 +0800

    fix: add absent typings

---
+++ packages/core/src/types/package-json.ts
@@ -0,0 +1,12 @@
+import { JsonObject, PackageJson } from "type-fest";
+
+export interface MineCatPackageJson extends PackageJson {
+  minecat: MinecatProjectConfig;
+}
+
+// TODO: should fill with Enum or strings @npmstudy
+export type MinecatProjectType = string;
+
+export interface MinecatProjectConfig extends JsonObject {
+  type: MinecatProjectType;
+}
