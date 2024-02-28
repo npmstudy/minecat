c927c473dbdb35cd40cb2c5ea7cfa894bffd5161
npmstudy <npmstudy@qq.com>
2024-02-29 00:00:01 +0800

    chore: tests

--- packages/minecat/src/index.ts
+++ packages/minecat/src/index.ts
@@ -1,10 +1,6 @@
-#!/usr/bin/env node
-
 import { cli } from "libargs";
 import { config } from "./config";
 
 export async function main(argv) {
   await cli(config, argv);
 }
-
-main(process.argv);
