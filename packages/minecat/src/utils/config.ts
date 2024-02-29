e043c9db7350680af45bbf164b0bf1d19e6f1491
citrus327 <citrus327@outlook.com>
2024-02-29 11:35:19 +0800

    feat: update config type

--- packages/minecat/src/utils/config.ts
+++ packages/minecat/src/utils/config.ts
@@ -1,24 +1,26 @@
 import { readFileSync, writeFileSync } from "node:fs";
-import { getSafeHome } from "./dir";
+import { getSafeHomeDir } from "./dir";
+import type { MinecatConfig } from "../types/minecat-config";
+import path from "node:path";
 
-export const DEFAULT_CONFIGS = {
+export const DEFAULT_CONFIGS: MinecatConfig = {
   "Node.js": "https://github.com/npmstudy/your-node-v20-monoreopo-project",
   React: "https://github.com/npmstudy/your-vite-react-monoreopo-project",
   Vue: "https://github.com/npmstudy/your-vite-react-monoreopo-project",
 };
 
 export function getConfigFile(): string {
-  return getSafeHome() + `/config.json`;
+  return path.join(getSafeHomeDir(), `/config.json`);
 }
 
 export function getConfig() {
   const configFile = getConfigFile();
-  // console.dir(configFile);
+
   try {
     return JSON.parse(readFileSync(configFile).toString());
   } catch (error) {
     if (error.errno === -2) {
-      // if config.json is not exist， write default config to it.
+      // if config.json is not exist, write default config to it.
       writeConfig(DEFAULT_CONFIGS);
       return DEFAULT_CONFIGS;
     } else {
