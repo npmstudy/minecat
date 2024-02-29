51fb00b90aead379f5ee89866be5068b2a71f111
whale2002 <1277180540@qq.com>
2024-02-29 07:03:00 +0000

    fix: 修复配置文件不存在时的bug

--- packages/minecat/src/cmd/config.ts
+++ packages/minecat/src/cmd/config.ts
@@ -1,53 +1,39 @@
-import prompts from "prompts";
-import debug from "debug";
-import { writeConfig, getConfig } from "../utils";
-const log = debug("minecat");
+import { readFileSync, writeFileSync, existsSync } from "node:fs";
+import { getSafeHomeDir } from "./dir";
+import type { MinecatConfig } from "../types/minecat-config";
+import path from "node:path";
 
-let cfgJson;
+export const DEFAULT_CONFIGS: MinecatConfig = {
+  "Node.js": "https://github.com/npmstudy/your-node-v20-monoreopo-project",
+  React: "https://github.com/npmstudy/your-vite-react-monoreopo-project",
+  Vue: "https://github.com/npmstudy/your-vite-react-monoreopo-project",
+};
 
-export async function ada(cmd) {
+export function getConfigFile(): string {
+  return path.join(getSafeHomeDir(), `/config.json`);
+}
+
+export function getConfig() {
+  const configFile = getConfigFile();
+
   try {
-    cfgJson = getConfig();
+    if (existsSync(configFile)) {
+      return JSON.parse(readFileSync(configFile).toString());
+    } else {
+      writeConfig(DEFAULT_CONFIGS);
+      return DEFAULT_CONFIGS;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function writeConfig(cfg) {
+  const configFile = getConfigFile();
+
+  try {
+    writeFileSync(configFile, JSON.stringify(cfg, null, 4));
   } catch (error) {
     console.dir(error);
-  }
-
-  if (cmd.input["_"][0] === "list") {
-    console.dir("---------current configuration--------");
-    return console.dir(cfgJson);
-  }
-
-  const questions: any = [
-    {
-      type: "text",
-      name: "newtpl",
-      initial: "",
-      message: "What is the name of your tpl config?",
-    },
-    {
-      type: "text",
-      name: "newrepo",
-      initial: "",
-      message: "What is the repo url of your config?",
-    },
-    {
-      type: "confirm",
-      name: "confirm",
-      initial: true,
-      message: (prev, values) => `Please confirm ?`,
-    },
-  ];
-  const response = await prompts(questions);
-  log(response);
-  log(cfgJson);
-
-  if (response["newrepo"].indexOf("http") !== -1) {
-    cfgJson[response["newtpl"]] = response["newrepo"];
-    await writeConfig(cfgJson);
-
-    console.dir(cfgJson);
-    console.dir("done!");
-  } else {
-    console.dir("请重新输入repo 地址");
   }
 }
