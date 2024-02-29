269ee96c683e3157184bb66e440c39031ed77320
citrus327 <citrus327@outlook.com>
2024-02-29 11:35:48 +0800

    feat!: remove write config in getSafeHomeDir fn

--- packages/minecat/src/utils/dir.ts
+++ packages/minecat/src/utils/dir.ts
@@ -1,21 +1,20 @@
 import { homedir } from "node:os";
 import fs from "node:fs";
-import { DEFAULT_CONFIGS, writeConfig } from "./config";
 
-export function getSafeHome(): string {
+export function getSafeHomeDir(): string {
   const home = homedir + `/.minecat`;
-
   if (!fs.existsSync(home)) {
     fs.mkdirSync(home);
-    writeConfig(DEFAULT_CONFIGS);
+    // writeConfig(DEFAULT_CONFIGS);
   }
 
   return home;
 }
 
 // 获取某个目录下面的所有文件夹
-export const getDirectories = (source: string) =>
-  fs
+export const getDirectories = (source: string) => {
+  return fs
     .readdirSync(source, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
+};
